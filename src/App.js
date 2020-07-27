721f57112ad3297b374706167bfbcf4fe9c76f58
Ming Lo <mlo@hotmail.co.nz>
2020-07-27 14:07:17 +1200

    to-do list test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props)
+
+    this.notes = [
+      {
+        id: 1,
+        text: 'Paint an illustration.'
+      },
+      {
+        id: 2,
+        text: 'Draw a user interface.'
+      },
+      {
+        id: 3,
+        text: 'App maker festival.'
+      },
+      {
+        id: 4,
+        text: 'Dua Lipa Auckland concert.'
+      },
+      {
+        id: 5,
+        text: 'Instrument recording, mixing.'
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="wrap">
+        <div className="container">
+          <h1>Hey Ming,<br />This is your to-do list.</h1>
+          <div className="notes">
+  
+            {
+              this.notes.map(note=>{
+
+                return (
+                  <div className="note">
+                    <div className="note-body">
+                      <i className="far fa-times-circle note-remove"></i>
+                      <div className="note-text">
+                      {note.text}
+                      </div>
+                    </div>
+                  </div>
+              )
+            })
+          }
+  
+              <div className="note new-note">
+                  <div className="form-group">
+                      <label htmlFor="note-input">
+                          Add a new note
+                      </label>
+                      <textarea className="form-control" id="note-input" rows="4"></textarea>
+                  </div>
+  
+                  <button type="submit" className="btn btn-primary"><i className="fas fa-plus-circle"></i></button>
+              </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
