8dbd5541e7aba959ed2df58c014ca021a1348b4b
Ming Lo <mlo@hotmail.co.nz>
2020-07-28 16:12:44 +1200

    todo_list

--- src/App.js
+++ src/App.js
@@ -8,26 +8,26 @@ class App extends Component{
 
     this.state = {
       notes: [
-        {
-          id: 1,
-          text: 'Paint an illustration.'
-        },
-        {
-          id: 2,
-          text: 'Draw a user interface.'
-        },
-        {
-          id: 3,
-          text: 'App maker festival.'
-        },
-        {
-          id: 4,
-          text: 'Dua Lipa Auckland concert.'
-        },
-        {
-          id: 5,
-          text: 'Instrument recording, mixing.'
-        }
+        // {
+        //   id: 1,
+        //   text: 'Paint an illustration.'
+        // },
+        // {
+        //   id: 2,
+        //   text: 'Draw a user interface.'
+        // },
+        // {
+        //   id: 3,
+        //   text: 'App maker festival.'
+        // },
+        // {
+        //   id: 4,
+        //   text: 'Dua Lipa Auckland concert.'
+        // },
+        // {
+        //   id: 5,
+        //   text: 'Instrument recording, mixing.'
+        // }
       ],
       noteInputValue : ''
     }
@@ -36,7 +36,31 @@ class App extends Component{
 
   handleNoteInputChange = (e)=>{
     this.setState({noteInputValue:e.target.value})
-    console.log(e.target)
+    // console.log(e.target)
+  }
+
+  handleAddButtonClick = (e)=>{
+    e.preventDefault()
+    var note = {
+      id: Date.now(),
+      text: this.state.noteInputValue
+    }
+
+    var newNotes = [note, ...this.state.notes]
+
+    this.setState({
+      notes: newNotes,
+      noteInputValue : ''
+    })
+  }
+
+  handleDeleteButtonClick = (e)=>{
+    var noteIdToDelete = e.target.id
+    var notes = this.state.notes
+
+    var filteredNotes = notes.filter((item)=>{
+      return item.id !== noteIdToDelete
+    })
   }
 
   render() {
@@ -52,7 +76,7 @@ class App extends Component{
                 return (
                   <div className="note" key={note.id}>
                     <div className="note-body">
-                      <i className="far fa-times-circle note-remove"></i>
+                      <i id={note.id} className="far fa-times-circle note-remove" onClick={this.handleDeleteButtonClick}></i>
                       <div className="note-text">
                       {note.text}
                       </div>
@@ -63,14 +87,16 @@ class App extends Component{
           }
   
               <div className="note new-note">
-                  <div className="form-group">
-                      <label htmlFor="note-input">
-                          Add a new note
-                      </label>
-                      <textarea className="form-control" id="note-input" rows="4" onChange={this.handleNoteInputChange}></textarea>
-                  </div>
-  
-                  <button type="submit" className="btn btn-primary"><i className="fas fa-plus-circle"></i></button>
+                  <form action="#">
+                    <div className="form-group">
+                        <label htmlFor="note-input">
+                            Add a new note
+                        </label>
+                        <textarea className="form-control" id="note-input" rows="4" value={this.state.noteInputValue} onChange={this.handleNoteInputChange}></textarea>
+                    </div>
+    
+                    <button type="submit" className="btn btn-primary" onClick={this.handleAddButtonClick}><i className="fas fa-plus-circle"></i></button>
+                  </form>
               </div>
           </div>
         </div>
